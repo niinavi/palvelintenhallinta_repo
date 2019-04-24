3172061d2ff2772a6873f885c547755f89662e92
niinavi <42713778+niinavi@users.noreply.github.com>
2019-04-24 11:19:06 +0300

    add verbose

--- minion.sh
+++ minion.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 # wget https://github.com/niinavi/palvelintenhallinta_repo/blob/master/minion.sh
 # bash minion.sh
+set -o verbose
 
 sudo apt-get update
 sudo apt-get -y install git salt-minion
