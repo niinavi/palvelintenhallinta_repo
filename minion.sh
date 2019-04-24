6adbb76a243ee4c7652ac8e9357d646609353783
niinavi <42713778+niinavi@users.noreply.github.com>
2019-04-24 11:38:41 +0300

    update

--- minion.sh
+++ minion.sh
@@ -1,8 +1,7 @@
 #!bin/bash
-# wget https://github.com/niinavi/palvelintenhallinta_repo/blob/master/minion.sh
+# wget https://raw.githubusercontent.com/niinavi/palvelintenhallinta_repo/master/minion.sh
 # bash minion.sh
-set -o verbose
 
 echo "Installing salt-minion and Git"
 sudo apt-get update
-sudo apt-get install salt-minion -y
+sudo apt-get -y install git salt-minion
